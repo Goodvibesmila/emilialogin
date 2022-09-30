75372441b4aaf134e104b84dea1f9b344c99c882
Emilia Johansson <johansson.emilia@hotmail.se>
2022-09-30 09:20:23 +0200

    built basic code js

---
+++ script.js
@@ -0,0 +1,31 @@
+const body = document.querySelector("body")
+
+const userName = "Fredrik";
+const passWord = "12345";
+
+function loggout(log) {
+    const loggoutbox = document.createElement("value")
+    loggoutbox.innerText = log
+
+    return loggoutbox;
+}
+
+
+button.addEventListener("click", function(log) {
+    const logg =  createValue("logga ut");
+});
+
+username.addEventListener("click", e => {
+    console.log("username")
+})
+
+password.addEventListener("click", e => {
+    console.log("password")
+})
+
+
+// Logga ut
+
+
+
+const form = document.getElementById("form");

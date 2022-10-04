f945a759aa51f0286ec3ee6eec8b67cb53adbca6
Emilia Johansson <johansson.emilia@hotmail.se>
2022-10-04 14:30:05 +0200

    changed

--- script.js
+++ script.js
@@ -1,31 +1,85 @@
-const body = document.querySelector("body")
+// Här är struktur på alla uppgifter jag vill hämta från HTML
 
-const userName = "Fredrik";
-const passWord = "12345";
+const form = document.querySelector(".form");
+const username = document.querySelector(".username");
+const user = document.querySelector(".user");
+const password = document.querySelector(".password");
+const word = document.querySelector(".word");
+const button = document.querySelector(".button");
+const logoutbutton = document.querySelector(".logout");
+const headlinemain = document.querySelector("main h2" );
+const paragraphmain = document.querySelector("main p" );
 
-function loggout(log) {
-    const loggoutbox = document.createElement("value")
-    loggoutbox.innerText = log
 
-    return loggoutbox;
+// Här kommer min information som skall hämtas och är sparad i lådor. Variabler.
+
+const users = [
+    {
+        userName: "Fredrik",
+        passWord: "12345",
+    },
+    {
+        userName: "Björn",
+        passWord: "222333",
+    },
+    {
+        userName: "Maria",
+        passWord: "112233",
+    }
+]
+
+
+// Här är mina kommandon för att säga åt webbläsare att något ska hända.
+
+
+button.addEventListener("click", control);
+logoutbutton.addEventListener("click", logout)
+
+
+// Här skapas min kontroll av lösen/användarnamn. Det görs genom en funktion, samt if/else.
+
+function control() {
+    for (let x of users) {
+    if(user.value === x.userName && word.value === x.passWord) {
+        rightuser(x.userName);
+
+    return
+    } 
+}
+wronguser();
+
+
+    //Sätt en loop runt ifsats.
 }
 
+// Här är en funktion med vad som ska hända om användarnamnet är godkänt.
 
-button.addEventListener("click", function(log) {
-    const logg =  createValue("logga ut");
-});
+function rightuser(rightname) {
+    form.style.display = "none";
+    headlinemain.innerText = "Välkommen " + rightname;
+    paragraphmain.innerText = "Du har lyckats att logga in";
+    logoutbutton.style.display = "block";
+}
 
-username.addEventListener("click", e => {
-    console.log("username")
-})
+// Här är en funktion med vad som ska hända om användarnamnet är fel.
 
-password.addEventListener("click", e => {
-    console.log("password")
-})
+function wronguser() {
+    headlinemain.innerText = "Försök igen.";
+    paragraphmain.innerText = "Du har skrivit fel lösenord eller användarnamn.";
+}
 
+// Här är logga ut sidan
 
-// Logga ut
+function logout() {
+    headlinemain.innerText = "Utloggad.";
+    paragraphmain.innerText = "Du har loggat ut.";
+    form.style.display = "block";
+    logoutbutton.style.display = "none";
+    user.value = "";
+    word.value = "";
+}
 
+// Här kommer kod för localstorage
 
-
-const form = document.getElementById("form");
+localStorage.setItem("storage", JSON.stringify(users));
+users = JSON.parse(localstorage.getItem(storage));

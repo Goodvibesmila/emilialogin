7b304bfc5d161e589b4ef23c4f37eddaf0fe3fc4
Emilia Johansson <johansson.emilia@hotmail.se>
2022-10-05 16:51:19 +0200

    fixed-css

--- script.js
+++ script.js
@@ -7,12 +7,12 @@ const password = document.querySelector(".password");
 const word = document.querySelector(".word");
 const button = document.querySelector(".button");
 const logoutbutton = document.querySelector(".logout");
-const headlinemain = document.querySelector("main h2" );
-const paragraphmain = document.querySelector("main p" );
-const moreusers = document.querySelector(".moreusers" );
-const newusername = document.querySelector(".newusername" );
-const newuserpassword = document.querySelector(".newuserpassword" );
-const newuserbutton = document.querySelector(".newuserbutton" );
+const headlinemain = document.querySelector("main h2");
+const paragraphmain = document.querySelector("main p");
+const moreusers = document.querySelector(".moreusers");
+const newusername = document.querySelector(".newusername");
+const newuserpassword = document.querySelector(".newuserpassword");
+const newuserbutton = document.querySelector(".newuserbutton");
 
 
 // Här kommer min information som skall hämtas och är sparad i lådor. Variabler.
@@ -35,7 +35,7 @@ let users = [
 // Här är mina kommandon för att säga åt webbläsare att något ska hända.
 
 button.addEventListener("click", control);
-logoutbutton.addEventListener("click", logout)
+logoutbutton.addEventListener("click", logout);
 newuserbutton.addEventListener("click", addnew, control);
 
 // Här skapas min kontroll av lösen/användarnamn. Det görs genom en funktion, samt if/else.
@@ -44,10 +44,11 @@ function control() {
     for (let x of users) {
     if(user.value === x.userName && word.value === x.passWord) {
         rightuser(x.userName);
-
+        
     return
     } 
 }
+
 wronguser();
 }
 
@@ -55,6 +56,7 @@ wronguser();
 
 function rightuser(rightname) {
     form.style.display = "none";
+    moreusers.style.display = "none";
     headlinemain.innerText = "Välkommen " + rightname;
     paragraphmain.innerText = "Du har lyckats att logga in";
     logoutbutton.style.display = "block";
@@ -71,12 +73,12 @@ function addnew(rightname) {
 
     users.push(addnewuser)
 
-    localStorage.setItem("storage", JSON.stringify(users));
-    headlinemain.innerText = "Välkommen " + rightname;
     moreusers.style.display = "none";
     form.style.display = "none";
+    headlinemain.innerText = "Välkommen " + rightname;
     paragraphmain.innerText = "Du har lyckats att logga in";
     logoutbutton.style.display = "block";
+    localStorage.setItem("storage", JSON.stringify(users));
 }
 
 // Här är en funktion med vad som ska hända om användarnamnet är fel.
